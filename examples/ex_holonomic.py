5071e966a134bbe2f00567ece0cf9dd48dafae7f
Ruben Van Parys <ruben.vanparys@kuleuven.be>
2015-12-03 10:45:53 +0100

    Updated examples + tutorial

--- examples/ex_holonomic.py
+++ examples/ex_holonomic.py
@@ -9,6 +9,8 @@ vehicle = Holonomic()
 # at start only constraints up to 1st derivative
 vehicle.options['boundary_smoothness']['initial'] = 1
 vehicle.set_options({'safety_distance': 0.1})
+# vehicle.set_options({'1storder_delay': True, 'time_constant': 0.1})
+# vehicle.set_input_disturbance(fc = 0.01, stdev = 0.05*np.ones(2))
 
 vehicle.set_initial_pose([-1.5, -1.5])
 vehicle.set_terminal_pose([2., 2.])
@@ -23,7 +25,7 @@ environment.add_obstacle(Obstacle({'position': [1.5, 0.5]}, shape=Circle(0.4),
                                   trajectory=trajectory))
 
 # create a point-to-point problem
-codegen = {'compileme': False, 'codegen': True, 'buildname': 'quadrotor'}
+codegen = {'compileme': False, 'codegen': True, 'buildname': 'holonomic'}
 problem = Point2point(environment, options={'codegen': codegen})
 
 # create simulator
@@ -36,8 +38,8 @@ simulator.plot.create('input')
 simulator.run()
 
 # show/save some results
-problem.plot.show_movie('2d', number_of_frames=100, repeat=False)
+simulator.plot.show_movie('2d', repeat=False)
 # problem.plot.save_movie('input', 5, 'holonomic')
 # problem.plot.show('input')
 
-plt.show(block=True)
+matplotlib.pyplot.show(block=True)
