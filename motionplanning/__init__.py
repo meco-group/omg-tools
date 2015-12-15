717c174ed0a63db9ddbafa0834405a476619eb82
rubenvanparys <ruben.vanparys@kuleuven.be>
2015-12-15 11:09:44 +0100

    update __init__

--- motionplanning/__init__.py
+++ motionplanning/__init__.py
@@ -14,8 +14,3 @@ from plots          import Plots
 from problem        import Simulator
 from problem        import Problem
 from point2point    import Point2point
-
-# develop
-# from admm import *
-# from spline_extra import *
-
