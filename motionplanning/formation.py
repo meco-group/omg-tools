6e79b3887f07799e39b5150f2e6083634b844467
Ruben Van Parys <ruben.vanparys@kuleuven.be>
2015-12-22 20:13:43 +0100

    updated formation class

--- motionplanning/formation.py
+++ motionplanning/formation.py
@@ -5,15 +5,21 @@ from point2point import Point2point
 class FormationPoint2point(ADMMProblem):
 
     def __init__(self, fleet, environment, options={}):
+        self.environment = environment
         problems = [Point2point(vehicle, environment, options)
                     for vehicle in fleet.vehicles]
         ADMMProblem.__init__(self, problems, options)
+        self.fleet = fleet
 
-        y = [vehicle.get_variable('y') for vehicle in self.vehicles]
-
-        Dx = [1., 2.]
-        for k in range(self.vehicles[0].n_y):
-            self.define_constraint(y[1][k] - y[0][k] - Dx[k], 0., 0.)
-            self.define_constraint(y[2][k] - y[1][k] - Dx[k], 0., 0.)
-            self.define_constraint(y[3][k] - y[2][k] - Dx[k], 0., 0.)
-            self.define_constraint(y[0][k] - y[3][k] - Dx[k], 0., 0.)
+        _couples = {veh: [] for veh in self.vehicles}
+        for veh in self.vehicles:
+            rp = self.fleet.get_rel_pos(veh)
+            for nghb in self.fleet.get_neighbors(veh):
+                if veh not in _couples[nghb]:
+                    _couples[veh].append(nghb)
+                    y_veh = veh.get_variable('y')
+                    y_nghb = nghb.get_variable('y')
+                    rel_pos = rp[nghb]
+                    for k in range(veh.n_y):
+                        self.define_constraint(
+                            y_veh[k] - y_nghb[k] - rel_pos[k], 0., 0.)
