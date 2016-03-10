d8a919cf1b3eb4e678bc14a04a427bacce265d5e
rubenvanparys <ruben.vanparys@kuleuven.be>
2016-03-10 15:15:22 +0100

    bug fixes concerning define_pose

--- omgtools/platform.py
+++ omgtools/platform.py
@@ -8,8 +8,8 @@ import numpy as np
 class Platform(Vehicle):
 
     def __init__(self, width=0.7, height=0.1, options={}, bounds={}, **kwargs):
-        Vehicle.__init__(self, n_y=1, degree=3, shape=Rectangle(width, height),
-                         options=options, order=2, **kwargs)
+        Vehicle.__init__(self, n_y=1, n_der=2, degree=3, order=1, shape=Rectangle(width, height),
+                         options=options, **kwargs)
         self.vmin = bounds['vmin'] if 'vmin' in bounds else -0.8
         self.vmax = bounds['vmax'] if 'vmax' in bounds else 0.8
         self.amin = bounds['amin'] if 'amin' in bounds else -2.
@@ -20,10 +20,12 @@ class Platform(Vehicle):
 
         y, dy, ddy = y[0, 0], y[0, 1], y[0, 2]
 
-        self.define_position([y, 0.])
-        self.define_input(dy)
-        self.define_state(y)
+        self.define_pose([y, 0.])
+        u = self.define_input(dy)
+        x = self.define_state(y)
         self.define_signal('a', ddy)
+        self.define_y([[x], [u]])
+        self.define_dstate([u])
 
         # define system constraints
         y = self.splines[0]
@@ -38,12 +40,12 @@ class Platform(Vehicle):
         self.define_constraint(ddy - (T**2)*self.amax, -inf, 0.)
 
     def set_initial_pose(self, position):
-        y = np.zeros((self.n_y, self.order+1))
+        y = np.zeros((self.n_y, self.n_der+1))
         y[:, 0] = position
         self.set_initial_condition(y)
 
     def set_terminal_pose(self, position):
-        y = np.zeros((self.n_y, self.order+1))
+        y = np.zeros((self.n_y, self.n_der+1))
         y[:, 0] = position
         self.set_terminal_condition(y)
 
@@ -53,15 +55,5 @@ class Platform(Vehicle):
         else:
             return self.shape.get_checkpoints([y[0], 0.])
 
-    def model_update(self, state, input):
-        dstate = input
-        return dstate
-
-    def get_y(self, state, input):
-        y = np.zeros((self.n_y, self.order+1))
-        y[:, 0] = state[:, 0]
-        y[:, 1] = input[:, 0]
-        return y, 1
-
     def draw(self, t=-1):
-        return np.vstack((self.path['position'][:, :, t])) + self.shape.draw()
+        return np.vstack((self.path['pose'][:, :, t])) + self.shape.draw()
