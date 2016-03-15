6bf3fffb9196599eb4982ad1f61e44bd2ea52c0b
Ruben Van Parys <ruben.vanparys@kuleuven.be>
2016-03-15 11:53:36 +0100

    reorganization in plots

--- omgtools/plots.py
+++ omgtools/plots.py
@@ -1,8 +1,8 @@
 import os
 import matplotlib
-matplotlib.use('TKAgg')
 import matplotlib.pyplot as plt
 import numpy as np
+matplotlib.use('TKAgg')
 
 # color definition (rgb)
 blue = [17., 110., 138.]
@@ -21,12 +21,12 @@ def mix_with_white(color, perc_white=80.):
 
 class Plots:
 
-    def __init__(self, fleet, environment, options={}):
-        self.fleet = fleet
-        self.vehicles = fleet.vehicles
-        self.environment = environment
+    def __init__(self, problem, options={}):
+        self.fleet = problem.fleet
+        self.vehicles = self.fleet.vehicles
+        self.environment = problem.environment
 
-        self.other_plt = {}
+        self.varia = {}
         self.plots = []
 
         # default colors
@@ -42,8 +42,8 @@ class Plots:
         while self.fleet.N > len(colors):
             colors.append(colors[ind])
             ind += 1
-        self.clr = {veh: colors[l] for l, veh in enumerate(self.vehicles)}
-        self.clr_w = {veh: mix_with_white(c) for veh, c in self.clr.items()}
+        self.col = {veh: colors[l] for l, veh in enumerate(self.vehicles)}
+        self.col_w = {veh: mix_with_white(c) for veh, c in self.col.items()}
 
     # ========================================================================
     # Plot options
@@ -52,70 +52,86 @@ class Plots:
     def set_options(self, options):
         if 'knots' in options and options['knots']:
             knots = {'symbol': 'x'}
-            knots['time'] = lambda veh, t: veh.trajectories_kn['time'][t]
-            knots['data'] = lambda veh, signal, k, t: veh.trajectories_kn[
-                signal][t][k, :, :]
-            self.other_plt['knots'] = knots
+            knots['time'] = lambda veh, t: veh.traj_storage_kn['time'][t]
+            knots['data'] = lambda veh, signal, k, t: veh.traj_storage_kn[
+                signal][t][k, :]
+            self.varia['knots'] = knots
         if 'prediction' in options and options['prediction']:
             prediction = {'symbol': 'o'}
-            prediction['time'] = lambda veh, t: veh.path['time'][t]
-            prediction['data'] = lambda veh, signal, k, t: veh.predictions[
+            prediction['time'] = lambda veh, t: veh.signals['time'][:, t]
+            prediction['data'] = lambda veh, signal, k, t: veh.pred_storage[
                 signal][t][k]
-            self.other_plt['prediction'] = prediction
+            self.varia['prediction'] = prediction
 
     # ========================================================================
     # Plot creation and initialization
     # ========================================================================
 
-    def create(self, signal, **kwargs):
-        if 'vehicles' in kwargs:
-            vehicles = kwargs['vehicles']
-        else:
-            vehicles = self.vehicles
-        if signal in ('2d', '2D'):
-            plots = self._init_2d_plot(vehicles)
-            self.plots.append(plots)
+    def show(self, signal, **kwargs):
+        vehicles = kwargs[
+            'vehicles'] if 'vehicles' in kwargs else self.vehicles
+        if signal == 'scene':
+            plots = [{'signal': signal, 'vehicles': vehicles,
+                      'type': '2d', 'kwargs': kwargs}]
         else:
             vehicle_types = self._sort_vehicles(vehicles)
             plots = []
             for vehicle_type, vehicles in vehicle_types.items():
-                plot = self._init_curve_plot(signal, vehicles, **kwargs)
-                self.plots.append(plot)
-                plots.append(plot)
+                plots.append({'signal': signal, 'vehicles': vehicles,
+                              'type': 'curve', 'kwargs': kwargs})
+        self.plots.extend(plots)
+        if 'time' in kwargs:
+            index = self._time2index(self.vehicles[0], kwargs['time'])
+        elif 'index' in kwargs:
+            index = kwargs['index']
+        else:
+            index = -1
+        no_update = kwargs['no_update'] if 'no_update' in kwargs else False
+        if hasattr(self.vehicles[0], 'signals') and not no_update:
+            self.update(t=index, plots=plots)
         return plots
 
-    def _init_curve_plot(self, signal, vehicles, **kwargs):
-        plt_traj, plt_path, plt_other = [], [], {}
-        for key in self.other_plt.keys():
-            plt_other[key] = []
-        sgn_length = vehicles[0].signal_length[signal]
-        if 'label' in kwargs:
-            label = kwargs['label']
-            if not isinstance(label, list):
-                label = [label]
+    def init(self, plot):
+        if plot['type'] == '2d':
+            plot.update(self._init_2d_plot(plot))
         else:
-            if sgn_length > 1:
-                label = [signal+str(k) for k in range(sgn_length)]
+            plot.update(self._init_curve_plot(plot))
+
+    def _init_curve_plot(self, plot):
+        signal, vehicles, kwargs = plot[
+            'signal'], plot['vehicles'], plot['kwargs']
+        plt_traj, plt_sign, plt_varia = [], [], {}
+        for key in self.varia.keys():
+            plt_varia[key] = []
+        size = vehicles[0].signals[signal].shape[0]
+        if 'label' in kwargs:
+            if not isinstance(kwargs['label'], list):
+                label = [kwargs['label']]
+            else:
+                label = kwargs['label']
+        else:
+            if size > 1:
+                label = [signal+str(k) for k in range(size)]
             else:
                 label = [signal]
-        figure, axis = plt.subplots(sgn_length, 1, squeeze=False)
-        for k in range(sgn_length):
+        figure, axis = plt.subplots(size, 1, squeeze=False)
+        for k in range(size):
             axis[k, 0].set_xlabel('t (s)')
             axis[k, 0].set_ylabel(label[k])
-        plt_traj = [[axis[k, 0].plot([], [], '-', color=self.clr_w[veh])[0]
-                     for k in range(sgn_length)] for veh in vehicles]
-        plt_path = [[axis[k, 0].plot([], [], '-', color=self.clr[veh])[0]
-                     for k in range(sgn_length)] for veh in vehicles]
-        for key, other in self.other_plt.items():
-            plt_other[key] = [[axis[k, 0].plot([], [], other['symbol'],
-                                               color=self.clr[veh])[0]
-                               for k in range(veh.signal_length[signal])]
+        plt_traj = [[axis[k, 0].plot([], [], '-', color=self.col_w[veh])[0]
+                     for k in range(size)] for veh in vehicles]
+        plt_sign = [[axis[k, 0].plot([], [], '-', color=self.col[veh])[0]
+                     for k in range(size)] for veh in vehicles]
+        for key, varia in self.varia.items():
+            plt_varia[key] = [[axis[k, 0].plot([], [], varia['symbol'],
+                                               color=self.col[veh])[0]
+                               for k in range(size)]
                               for veh in vehicles]
-        return {'type': 'curve', 'figure': figure, 'axis': axis,
-                'trajectory': plt_traj, 'path': plt_path,
-                'other': plt_other, 'signal': signal, 'vehicles': vehicles}
+        return {'figure': figure, 'axis': axis, 'plt_traj': plt_traj,
+                'plt_sign': plt_sign, 'plt_varia': plt_varia, 'size': size}
 
-    def _init_2d_plot(self, vehicles):
+    def _init_2d_plot(self, plot):
+        vehicles = plot['vehicles']
         figure = plt.figure()
         axis = figure.add_subplot(111)
         canvas_lim = self.environment.get_canvas_limits()
@@ -124,14 +140,14 @@ class Plots:
         plt_2d = {}
         plt_2d['environment'] = [
             axis.plot([], [], 'k-')[0] for l in range(self.environment.No)]
-        plt_2d['trajectory'] = [axis.plot(
-            [], [], '-', color=self.clr_w[veh])[0] for veh in vehicles]
-        plt_2d['path'] = [axis.plot(
-            [], [], '-', color=self.clr[veh])[0] for veh in vehicles]
-        plt_2d['vehicle'] = [axis.plot(
-            [], [], '-', color=self.clr[veh])[0] for veh in vehicles]
-        return {'type': '2d', 'figure': figure, 'axis': axis,
-                '2d': plt_2d, 'vehicles': vehicles}
+        plt_2d['pos_traj'] = [axis.plot(
+            [], [], '-', color=self.col_w[veh])[0] for veh in vehicles]
+        plt_2d['pos_sign'] = [axis.plot(
+            [], [], '-', color=self.col[veh])[0] for veh in vehicles]
+        # plt_2d['vehicle'] = [[axis.plot([], [], '-', color=self.col[veh])[0] for shape in veh.shapes] for veh in vehicles]
+        plt_2d['vehicle'] = [
+            axis.plot([], [], '-', color=self.col[veh])[0] for veh in vehicles]
+        return {'figure': figure, 'axis': axis, 'plt_2d': plt_2d}
 
     def _sort_vehicles(self, vehicles):
         vehicle_types = {}
@@ -151,33 +167,36 @@ class Plots:
         plots = self.plots if (plots is None) else plots
         plots = plots if isinstance(plots, list) else [plots]
         for plot in plots:
+            if 'figure' not in plot:
+                self.init(plot)
             if plot['type'] == 'curve':
                 self._update_curve_plot(plot, t)
             if plot['type'] == '2d':
                 self._update_2d_plot(plot, t)
 
     def _update_curve_plot(self, plot, t=-1):
-        plt_traj = plot['trajectory']
-        plt_path = plot['path']
-        plt_other = plot['other']
+        plt_traj = plot['plt_traj']
+        plt_sign = plot['plt_sign']
+        plt_varia = plot['plt_varia']
+        size = plot['size']
         signal, vehicles = plot['signal'], plot['vehicles']
         for l, veh in enumerate(vehicles):
-            for k in range(veh.signal_length[signal]):
+            for k in range(size):
                 plt_traj[l][k].set_data(
-                    veh.trajectories['time'][t].ravel(),
-                    veh.trajectories[signal][t][k, :, :].ravel())
+                    veh.traj_storage['time'][t].ravel(),
+                    veh.traj_storage[signal][t][k, :].ravel())
                 if t == -1:
-                    plt_path[l][k].set_data(
-                        veh.path['time'].ravel(),
-                        veh.path[signal][k, :, :].ravel())
+                    plt_sign[l][k].set_data(
+                        veh.signals['time'].ravel(),
+                        veh.signals[signal][k, :].ravel())
                 else:
-                    plt_path[l][k].set_data(
-                        veh.path['time'][:t+1].ravel(),
-                        veh.path[signal][k, :, :t+1].ravel())
-                for key, other in self.other_plt.items():
-                    plt_other[key][l][k].set_data(
-                        other['time'](veh, t).ravel(),
-                        other['data'](veh, signal, k, t).ravel())
+                    plt_sign[l][k].set_data(
+                        veh.signals['time'][:, :t+1].ravel(),
+                        veh.signals[signal][k, :t+1].ravel())
+                for key, varia in self.varia.items():
+                    plt_varia[key][l][k].set_data(
+                        varia['time'](veh, t).ravel(),
+                        varia['data'](veh, signal, k, t).ravel())
         fig, ax = plot['figure'], plot['axis']
         for i in range(ax.shape[0]):
             for j in range(ax.shape[1]):
@@ -186,22 +205,22 @@ class Plots:
         fig.canvas.draw()
 
     def _update_2d_plot(self, plot, t=-1):
-        plt_2d, vehicles = plot['2d'], plot['vehicles']
+        plt_2d, vehicles = plot['plt_2d'], plot['vehicles']
         environment = self.environment.draw(t)
         for l, env in enumerate(environment):
             plt_2d['environment'][l].set_data(
                 env[0, :].ravel(), env[1, :].ravel())
         for l, veh in enumerate(vehicles):
             if t == -1:
-                pos_path = veh.path['pose'][:, :]
+                pos_sign = veh.signals['position'][:, :]
             else:
-                pos_path = veh.path['pose'][:, :, :t+1]
-            pos_traj = veh.trajectories['pose'][t]
+                pos_sign = veh.signals['position'][:, :t+1]
+            pos_traj = veh.traj_storage['position'][t]
             veh_cnt = veh.draw(t)
-            plt_2d['trajectory'][l].set_data(pos_traj[0, :].ravel(),
-                                             pos_traj[1, :].ravel())
-            plt_2d['path'][l].set_data(pos_path[0, :].ravel(),
-                                       pos_path[1, :].ravel())
+            plt_2d['pos_traj'][l].set_data(pos_traj[0, :].ravel(),
+                                           pos_traj[1, :].ravel())
+            plt_2d['pos_sign'][l].set_data(pos_sign[0, :].ravel(),
+                                           pos_sign[1, :].ravel())
             plt_2d['vehicle'][l].set_data(veh_cnt[0].ravel(),
                                           veh_cnt[1].ravel())
         plot['figure'].canvas.draw()
@@ -211,22 +230,12 @@ class Plots:
     # ========================================================================
 
     def _time2index(self, vehicle, time):
-        time_axis = vehicle.path['time']
-        Ts = time_axis[1] - time_axis[0]
-        for k, t in enumerate(time_axis):
+        time_axis = vehicle.signals['time']
+        Ts = time_axis[:, 1] - time_axis[:, 0]
+        for k, t in enumerate(time_axis[0, :]):
             t = np.round(t, 6)
             if (t <= time) and (time < (t+Ts)) and ((time-t) <= (t+Ts-time)):
                 return k
-
-    def show(self, signal, **kwargs):
-        if 'time' in kwargs:
-            index = self._time2index(self.vehicles[0], kwargs['time'])
-        elif 'index' in kwargs:
-            index = kwargs['index']
-        else:
-            index = -1
-        plot = self.create(signal, **kwargs)
-        self.update(t=index, plots=plot)
 
     def save(self, signal, name='plot', path='images/', **kwargs):
         from matplotlib2tikz import save as tikz_save
@@ -239,16 +248,17 @@ class Plots:
         tikz_save(directory+'/'+name+'.tikz', figurewidth=figurewidth)
 
     def show_movie(self, signal, repeat=False, **kwargs):
-        t = self.vehicles[0].path['time']
+        t = self.vehicles[0].signals['time']
         if ('number_of_frames' in kwargs and
-                kwargs['number_of_frames'] <= (len(t)-1)):
+                kwargs['number_of_frames'] <= (t.shape[1]-1)):
             number_of_frames = kwargs['number_of_frames']
         else:
-            number_of_frames = len(t)-1
-        subsample = (len(t)-1)/(number_of_frames-1)
-        plot = self.create(signal, **kwargs)
+            number_of_frames = t.shape[1]-1
+        subsample = (t.shape[1]-1)/(number_of_frames-1)
+        kwargs['no_update'] = True
+        plot = self.show(signal, **kwargs)
         while True:
-            for k in range(0, len(t)-1, subsample):
+            for k in range(0, t.shape[1]-1, subsample):
                 self.update(k, plots=plot)
             if not repeat:
                 break
@@ -258,19 +268,20 @@ class Plots:
         directory = path + name
         if not os.path.isdir(directory):
             os.makedirs(directory)
-        t = self.vehicles[0].path['time']
+        t = self.vehicles[0].signals['time']
         if ('number_of_frames' in kwargs and
-                kwargs['number_of_frames'] <= (len(t)-1)):
+                kwargs['number_of_frames'] <= (t.shape[1]-1)):
             number_of_frames = kwargs['number_of_frames']
         else:
-            number_of_frames = len(t)-1
+            number_of_frames = t.shape[1]-1
         figwidth = kwargs['figurewidth'] if 'figurewidth' in kwargs else '8cm'
-        subsample = (len(t)-1)/(number_of_frames-1)
-        plot = self.create(signal, **kwargs)
+        subsample = (t.shape[1]-2)/(number_of_frames-1)
+        kwargs['no_update'] = True
+        plot = self.show(signal, **kwargs)
         cnt = 0
-        if signal in ('2d', '2D'):
+        if signal in ('scene'):
             plt.axis('off')
-        for k in range(0, len(t)-1, subsample):
+        for k in range(0, t.shape[1]-1, subsample):
             self.update(k, plots=plot)
             tikz_save(
                 directory+'/'+name+'_'+str(cnt)+'.tikz', figurewidth=figwidth)
