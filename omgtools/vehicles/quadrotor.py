474978dc296a2b28e1a3b4d977faca50fe369e5c
rubenvanparys <ruben.vanparys@kuleuven.be>
2016-03-15 20:19:39 +0100

    implement quadrotor with new interface

--- omgtools/vehicles/quadrotor.py
+++ omgtools/vehicles/quadrotor.py
@@ -1,68 +1,124 @@
 from vehicle import Vehicle
-from ..basics.shape import Quad
+from ..basics.shape import Circle
+from ..basics.spline_extra import sample_splines
+from casadi import inf
 import numpy as np
-from casadi import inf, sqrt, arctan2, sin, cos
 
 
 class Quadrotor(Vehicle):
 
-    def __init__(self, radius=0.2, options={}, bounds={},  **kwargs):
-        Vehicle.__init__(self, n_y=2, n_der=3, degree=4, order=2, shape=Quad(
-            radius), options=options, **kwargs)
+    def __init__(self, radius=0.2, options={}, bounds={}):
+        Vehicle.__init__(
+            self, n_spl=2, degree=4, shapes=Circle(radius), options=options)
+        self.radius = radius
         self.u1min = bounds['u1min'] if 'u1min' in bounds else 1.
         self.u1max = bounds['u1max'] if 'u1max' in bounds else 15.
         self.u2min = bounds['u2min'] if 'u2min' in bounds else -8.
         self.u2max = bounds['u2max'] if 'u2max' in bounds else 8.
         self.g = 9.81
+        # time horizon
+        self.T = self.define_symbol('T')
 
-        # define physical signals
-        y = self.get_signal('y')
+    def define_trajectory_constraints(self, splines):
+        x, y = splines
+        ddx, ddy = x.derivative(2), y.derivative(2)
+        dddx, dddy = x.derivative(3), y.derivative(3)
+        g_tf = self.g*(self.T**2)
+        self.define_constraint(-(ddx**2 + (ddy+g_tf)**2) +
+                               (self.T**4)*self.u1min**2, -inf, 0.)
+        self.define_constraint(
+            (ddx**2 + (ddy+g_tf)**2) - (self.T**4)*self.u1max**2, -inf, 0.)
+        self.define_constraint(-(dddx*(ddy+g_tf) - ddx*dddy) +
+                               (ddx**2 + (ddy + g_tf)**2)*(self.T*self.u2min), -inf, 0.)
+        self.define_constraint(
+            (dddx*(ddy+g_tf) - ddx*dddy) - (ddx**2 + (ddy + g_tf)**2)*(self.T*self.u2max), -inf, 0.)
 
-        y0, dy0, ddy0, dddy0 = y[0, 0], y[0, 1], y[0, 2], y[0, 3]
-        y1, dy1, ddy1, dddy1 = y[1, 0], y[1, 1], y[1, 2], y[1, 3]
+    def get_initial_constraints(self, splines):
+        spl0 = self.define_parameter('spl0', 2)
+        dspl0 = self.define_parameter('dspl0', 2)
+        ddspl0 = self.define_parameter('ddspl0', 2)
+        x, y = splines
+        dx, dy = x.derivative(), y.derivative()
+        ddx, ddy = x.derivative(2), y.derivative(2)
+        return [(x, spl0[0]), (y, spl0[1]),
+                (dx, self.T*dspl0[0]), (dy, self.T*dspl0[1]),
+                (ddx, (self.T**2)*ddspl0[0]), (ddy, (self.T**2)*ddspl0[1])]
 
-        u1 = sqrt(ddy0**2 + (ddy1+self.g)**2)
-        u2 = (dddy0*(ddy1 + self.g) - ddy0*dddy1) / \
-            ((ddy1 + self.g)**2 + ddy0**2)
-        theta = arctan2(ddy0, (ddy1 + self.g))
+    def get_terminal_constraints(self, splines):
+        position = self.define_parameter('positionT', 2)
+        x, y = splines
+        return [(x, position[0]), (y, position[1])]
 
-        self.define_pose([y0, y1])
-        u1, u2 = self.define_input([u1, u2])
-        x, z, th, dx, dz = self.define_state([y0, y1, theta, dy0, dy1])
-        self.define_y([[x, z], [dx, dz], [u1*sin(th), u1*cos(th) - self.g]])
-        self.define_dstate([dx, dz, u2, u1*sin(th), u1*cos(th) - self.g])
+    def set_initial_conditions(self, position):
+        self.prediction['state'] = np.r_[position, np.zeros(3)].T
+        self.prediction['dspl'] = np.zeros(2)
+        self.prediction['ddspl'] = np.zeros(2)
 
-        # define system constraints
-        y0, y1 = self.splines[0], self.splines[1]
-        dy0, dy1 = y0.derivative(), y1.derivative()
-        ddy0, ddy1 = y0.derivative(2), y1.derivative(2)
-        dddy0, dddy1 = y0.derivative(3), y1.derivative(3)
-        # this is a symbol which needs to be defined by another OptiLayer
-        # object as variable or parameter
-        T = self.define_symbol('T')
-        g_tf = self.g*(T**2)
+    def set_terminal_conditions(self, position):
+        self.positionT = position
 
-        self.define_constraint(-(ddy0**2 + (ddy1+g_tf) ** 2) +
-                               (T**4)*self.u1min**2, -inf, 0.)
-        self.define_constraint((ddy0**2 + (ddy1+g_tf)**2) -
-                               (T**4)*self.u1max**2, -inf, 0.)
-        self.define_constraint(-(dddy0*(ddy1+g_tf) - ddy0*dddy1) +
-                               (ddy0**2 + (ddy1 + g_tf)**2)*(T*self.u2min),
-                               -inf, 0.)
-        self.define_constraint((dddy0*(ddy1+g_tf) - ddy0*dddy1) -
-                               (ddy0**2 + (ddy1 + g_tf)**2)*(T*self.u2max),
-                               -inf, 0.)
+    def get_init_spline_value(self):
+        init_value = np.zeros((len(self.basis), 2))
+        pos0 = self.prediction['state'][:2]
+        posT = self.positionT
+        for k in range(2):
+            init_value[:, k] = np.r_[pos0[k]*np.ones(self.degree), np.linspace(
+                pos0[k], posT[k], len(self.basis) - 2*self.degree), posT[k]*np.ones(self.degree)]
+        return init_value
 
-    def set_initial_pose(self, position):
-        y = np.zeros((self.n_y, self.n_der+1))
-        y[:, 0] = position
-        self.set_initial_condition(y)
+    def check_terminal_conditions(self):
+        if (np.linalg.norm(self.signals['position'][:, -1] - self.positionT) > 1.e-2 or
+                np.linalg.norm(self.signals['dspl'][:, -1])) > 1.e-2:
+            return False
+        else:
+            return True
 
-    def set_terminal_pose(self, position):
-        y = np.zeros((self.n_y, self.n_der+1))
-        y[:, 0] = position
-        self.set_terminal_condition(y)
+    def set_parameters(self, current_time):
+        parameters = {}
+        parameters['spl0'] = self.prediction['state'][:2]
+        parameters['dspl0'] = self.prediction['dspl']
+        parameters['ddspl0'] = self.prediction['ddspl']
+        parameters['positionT'] = self.positionT
+        return parameters
+
+    def define_collision_constraints(self, hyperplanes, room_lim, splines):
+        x, y = splines[0], splines[1]
+        self.define_collision_constraints_2d(hyperplanes, room_lim, [x, y])
+
+    def splines2signals(self, splines, time):
+        signals = {}
+        x, y = splines[0], splines[1]
+        dx, dy = x.derivative(), y.derivative()
+        ddx, ddy = x.derivative(2), y.derivative(2)
+        dddx, dddy = x.derivative(3), y.derivative(3)
+
+        x_s, y_s = sample_splines([x, y], time)
+        dx_s, dy_s = sample_splines([dx, dy], time)
+        ddx_s, ddy_s = sample_splines([ddx, ddy], time)
+        dddx_s, dddy_s = sample_splines([dddx, dddy], time)
+
+        theta = np.arctan2(ddx_s, ddy_s + self.g)
+        u1 = np.sqrt(ddx_s**2 + (ddy_s + self.g)**2)
+        u2 = (dddx_s*(ddy_s + self.g) - ddx_s*dddy_s) / \
+            ((ddy_s + self.g)**2 + ddx_s**2)
+        signals['state'] = np.c_[x_s, y_s, dx_s, dy_s, theta].T
+        signals['input'] = np.c_[u1, u2].T
+        signals['position'] = np.c_[x_s, y_s].T
+        signals['dspl'] = np.c_[dx_s, dy_s].T
+        signals['ddspl'] = np.c_[ddx_s, ddy_s].T
+        return signals
+
+    def ode(self, state, input):
+        theta = state[4]
+        u1, u2 = input[0], input[1]
+        return np.r_[state[2:4], u1*np.sin(theta), u1*np.cos(theta)-self.g, u2].T
 
     def draw(self, t=-1):
-        return (self.path['pose'][:, :, t] +
-                self.shape.draw(-self.path['state'][2, :, t]))
+        theta = -self.signals['state'][4, t]
+        cth, sth = np.cos(theta), np.sin(theta)
+        rot = np.array([[cth, -sth], [sth, cth]])
+        r = self.radius
+        h, rw = 0.2*r, (1./3.)*r
+        plt_x = [r, r-2*rw, r-rw, r-rw, -r+rw, -r+rw, -r, -r+2*rw]
+        plt_y = [h, h, h, 0, 0, h, h, h]
+        return [np.c_[self.signals['position'][:, t]] + rot.dot(np.vstack((plt_x, plt_y)))]
