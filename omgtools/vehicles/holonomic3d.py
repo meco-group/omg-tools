003ba8ae60e0f2b5893928c7e18cb53a2e396c2b
Ruben Van Parys <ruben.vanparys@kuleuven.be>
2016-03-21 11:45:15 +0100

    added licenses

--- omgtools/vehicles/holonomic3d.py
+++ omgtools/vehicles/holonomic3d.py
@@ -1,5 +1,23 @@
+# This file is part of OMG-tools.
+#
+# OMG-tools -- Optimal Motion Generation-tools
+# Copyright (C) 2016 Ruben Van Parys & Tim Mercy, KU Leuven.
+# All rights reserved.
+#
+# OMG-tools is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
 from vehicle import Vehicle
-from ..basics.shape import Circle
 from ..basics.spline_extra import sample_splines
 from casadi import inf
 import numpy as np
