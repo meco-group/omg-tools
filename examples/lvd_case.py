1b48c0fae79a0de6760be7c7211e65d828d2ba12
rubenvanparys <ruben.vanparys@kuleuven.be>
2016-04-07 09:51:19 +0200

    updated lvd case after comments Wim

--- examples/lvd_case.py
+++ examples/lvd_case.py
@@ -1,16 +1,20 @@
 # Implementation of pick and place case of LVD
 # questions: ruben.vanparys@kuleuven.be
-import sys, os
+
+import sys
+import os
 sys.path.insert(0, os.getcwd()+'/../')
 import matplotlib
 from omgtools import *
 from omgtools.vehicles.lvd_machine import LVD
 
+save = True
+
 # create plate
 # limits on position mid-point plate
-bounds = {'smin': [None, -0.95, 0.405], 'smax': [None, -0.55, None]}
+bounds = {'smin': [None, -0.95, 0.405], 'smax': [None, -0.55, 1.5]}
 # shutdown room constraints (we use constraints on position)
-options = {'room_constraint': None}
+options = {'room_constraint': None, 'knot_intervals': 11}
 # shape of plate
 shape = Plate(Rectangle(3., 1.5), 0.01)
 plate = LVD(shape, options=options, bounds=bounds)
@@ -23,14 +27,27 @@ environment = Environment(
     room={'shape': Cuboid(10., 3.7, 2.3), 'position': [5., -0.35, 1.15]})
 
 # obstacles for collision avoidance
-cuboid1 = Cuboid(6.950, 1.7, 1.0)
-cuboid1.radius = 0.005  # for safety
-cuboid = Obstacle({'position': [3.475, -0.65, 0.5]}, cuboid1, draw=False)
-frame = Obstacle(
-    {'position': [0.165, 0.0, 1.85]}, Cuboid(0.33, 3.0, 0.9), avoid=True)
-environment.add_obstacle([cuboid, frame])
+cuboids = False  # False -> a bit faster
+if cuboids:
+    shape1 = Cuboid(6.950, 1.7, 1.0)
+    shape1.radius = 0.005  # for safety
+    shape2 = Cuboid(0.85, 3.0, 1.0)
+else:
+    vertices1 = np.c_[[3.475, -0.85, -0.5], [3.475, 0.85, -0.5],
+                      [3.475, -0.85,  0.5], [3.475, 0.85,  0.5],
+                      [-3.475, -0.85,  0.5], [-3.475, 0.85,  0.5]]
+    shape1 = Polyhedron3D(vertices1, 0.005)
+    vertices2 = np.c_[[0.425, 1.5, -0.5], [0.425, 1.5, 0.5],
+                      [0.425, -1.5, -0.5], [0.425, -1.5, 0.5]]
+    shape2 = Polyhedron3D(vertices2, 0.001)
+
+obstacle1 = Obstacle({'position': [3.475, -0.65, 0.5]}, shape1, draw=False)
+obstacle2 = Obstacle({'position': [0.425, 0.0, 1.6]}, shape2, draw=False)
+environment.add_obstacle([obstacle1, obstacle2])
 
 # obstacles just for drawing
+frame = Obstacle(
+    {'position': [0.165, 0.0, 1.85]}, Cuboid(0.33, 3.0, 0.9), avoid=False)
 table = Obstacle(
     {'position': [1.5, 0.0, 0.5]}, Cuboid(3., 3., 1.), avoid=False)
 cover = Obstacle(
@@ -43,11 +60,12 @@ pilar = Obstacle(
     {'position': [3.2, -1.925, 0.75]}, Cuboid(0.3, 0.45, 1.5), avoid=False)
 measure = Obstacle(
     {'position': [6.875, -1.325, 0.4]}, Cuboid(0.15, 0.15, 0.8), avoid=False)
-environment.add_obstacle([table, cover, beam, leg, pilar, measure])
+environment.add_obstacle([frame, table, cover, beam, leg, pilar, measure])
 
 # create a point-to-point problem
 problem = Point2point(plate, environment, freeT=True)
-problem.set_options({'solver': {'ipopt.linear_solver': 'ma57'}, 'horizon_time': 12.})
+problem.set_options(
+    {'solver': {'ipopt.linear_solver': 'ma57'}, 'horizon_time': 10.})
 problem.init()
 
 # create simulator
@@ -65,14 +83,22 @@ simulator.plot.show(
     'jerk', label=['dddx (m/s^3)', 'dddy (m/s^3)', 'dddz (m/s^3)'])
 simulator.plot.show_movie(
     'scene', number_of_frames=40, repeat=True, view=[60, 45])
+simulator.plot.show_movie(
+    'scene', number_of_frames=40, repeat=True, view=[0, 90])
 
 # save results
-# simulator.plot.save('state', 'position', label=['x (m)', 'y (m)', 'z (m)'], figurewidth='15cm', figureheight='4cm')
-# simulator.plot.save('velocity', 'velocity', label=['dx (m/s)', 'dy (m/s)' ,'dz (m/s)'], figurewidth='15cm', figureheight='4cm')
-# simulator.plot.save('acceleration', 'acceleration', label=['ddx (m/s^2)', 'ddy (m/s^2)' ,'ddz (m/s^2)'], figurewidth='15cm', figureheight='4cm')
-# simulator.plot.save('jerk', 'jerk', label=['dddx (m/s^3)', 'dddy (m/s^3)' ,'dddz (m/s^3)'], figurewidth='15cm', figureheight='4cm')
-# simulator.plot.save('scene')
+if save:
+    simulator.plot.save('state', 'position', label=[
+                        'x (m)', 'y (m)', 'z (m)'], figurewidth='15cm', figureheight='4cm')
+    simulator.plot.save('velocity', 'velocity', label=[
+                        'dx (m/s)', 'dy (m/s)', 'dz (m/s)'], figurewidth='15cm', figureheight='4cm')
+    simulator.plot.save('acceleration', 'acceleration', label=[
+                        'ddx (m/s^2)', 'ddy (m/s^2)', 'ddz (m/s^2)'], figurewidth='15cm', figureheight='4cm')
+    simulator.plot.save('jerk', 'jerk', label=[
+                        'dddx (m/s^3)', 'dddy (m/s^3)', 'dddz (m/s^3)'], figurewidth='15cm', figureheight='4cm')
 
-# simulator.plot.show('scene', time=0, view=[30, 60], limits=[
-#                     [0., 10.], [-4.65, 5.35], [-4.85, 6.15]])
+    simulator.plot.save_movie('scene', 'scene1', number_of_frames=40, view=[30, 60], limits=[
+        [0., 10.], [-4.65, 5.35], [-4.85, 6.15]])
+    simulator.plot.save_movie('scene', 'scene2', number_of_frames=40, view=[0, 90], limits=[
+        [0., 10.], [-4.65, 5.35], [-4.85, 6.15]])
 matplotlib.pyplot.show(block=True)
--- omgtools/vehicles/lvd_machine.py
+++ omgtools/vehicles/lvd_machine.py
@@ -48,23 +48,29 @@ class LVD(Vehicle):
     def get_initial_constraints(self, splines):
         state0 = self.define_parameter('state0', 3)
         input0 = self.define_parameter('input0', 3)
+        acceleration0 = self.define_parameter('acceleration0', 3)
         x, y, z = splines
         dx, dy, dz = x.derivative(), y.derivative(), z.derivative()
+        ddx, ddy, ddz = x.derivative(2), y.derivative(2), z.derivative(2)
         return [(x, state0[0]), (y, state0[1]), (z, state0[2]),
                 (dx, self.T*input0[0]), (dy, self.T*input0[1]),
-                (dz, self.T*input0[2])]
+                (dz, self.T*input0[2]), (ddx, (self.T**2)*acceleration0[0]),
+                (ddy, (self.T**2)*acceleration0[1]), (ddz, (self.T**2)*acceleration0[2])]
 
     def get_terminal_constraints(self, splines):
         position = self.define_parameter('positionT', 3)
         x, y, z = splines
         dx, dy, dz = x.derivative(), y.derivative(), z.derivative()
+        ddx, ddy, ddz = x.derivative(2), y.derivative(2), z.derivative(2)
         term_con = [(x, position[0]), (y, position[1]), (z, position[2])]
-        term_con_der = [(dx, 0.), (dy, 0.), (dz, 0.)]
+        term_con_der = [
+            (dx, 0.), (dy, 0.), (dz, 0.), (ddx, 0.), (ddy, 0.), (ddz, 0.)]
         return [term_con, term_con_der]
 
     def set_initial_conditions(self, position, input=np.zeros(3)):
         self.prediction['state'] = position
         self.prediction['input'] = input
+        self.prediction['acceleration'] = np.zeros(3)
 
     def set_terminal_conditions(self, position):
         self.positionT = position
@@ -79,8 +85,8 @@ class LVD(Vehicle):
             pos0[0], posT[0], len(self.basis) - end_cont - 2 - breakpoint), posT[0]*np.ones(end_cont)]
         init_value[:, 1] = np.r_[pos0[1]*np.ones(2), np.linspace(
             pos0[1], posT[1], len(self.basis) - end_cont - 2), posT[1]*np.ones(end_cont)]
-        init_value[:, 2] = np.r_[pos0[2]*np.ones(2), np.linspace(pos0[2], 1.5*posT[2], breakpoint), np.linspace(
-            1.5*posT[2], posT[2], len(self.basis)-breakpoint - end_cont - 2), posT[2]*np.ones(end_cont)]
+        init_value[:, 2] = np.r_[pos0[2]*np.ones(2), np.linspace(pos0[2], 1.2*posT[2], breakpoint), np.linspace(
+            1.2*posT[2], posT[2], len(self.basis)-breakpoint - end_cont - 2), posT[2]*np.ones(end_cont)]
         return init_value
 
     def check_terminal_conditions(self):
@@ -94,6 +100,7 @@ class LVD(Vehicle):
         parameters = {}
         parameters['state0'] = self.prediction['state']
         parameters['input0'] = self.prediction['input']
+        parameters['acceleration0'] = self.prediction['acceleration']
         parameters['positionT'] = self.positionT
         return parameters
 
