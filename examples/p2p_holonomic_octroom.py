f00744f8163c206ef7d1d0bcd8b510c50c82f64e
u0098366 <tim.mercy@kuleuven.be>
2016-04-14 15:50:01 +0200

    Added example with octagonal room

---
+++ examples/p2p_holonomic_octroom.py
@@ -0,0 +1,58 @@
+# This file is part of OMG-tools.
+#
+# OMG-tools -- Optimal Motion Generation-tools
+# Copyright (C) 2016 Ruben Van Parys & Tim Mercy, KU Leuven.
+# All rights reserved.
+#
+# OMG-tools is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+from omgtools import *
+
+# create vehicle
+vehicle = Holonomic()
+vehicle.set_options({'safety_distance': 0.1})
+# vehicle.set_options({'1storder_delay': True, 'time_constant': 0.1})
+# vehicle.set_options({'input_disturbance': {'fc':0.01, 'stdev':0.05*np.ones(2)}})
+
+vehicle.set_initial_conditions([-1.5, -1.5])
+vehicle.set_terminal_conditions([1.0, 1.5])
+
+# create environment
+environment = Environment(room={'shape': RegularPolyhedron(2.5, 8)})
+rectangle = Rectangle(width=3., height=0.2)
+
+environment.add_obstacle(Obstacle({'position': [-2.1, -0.5]}, shape=rectangle))
+environment.add_obstacle(Obstacle({'position': [1.7, -0.5]}, shape=rectangle))
+trajectories = {'velocity': {2: [-0.15, 0.0], 4: [0., 0.15]}}
+environment.add_obstacle(Obstacle({'position': [1.5, 0.5]}, shape=Circle(0.4),
+                                  trajectories=trajectories))
+
+# create a point-to-point problem
+problem = Point2point(vehicle, environment, freeT=False)
+# problem.set_options({'solver': {'ipopt.linear_solver': 'ma57'}})
+problem.init()
+
+# create simulator
+simulator = Simulator(problem)
+simulator.plot.set_options({'knots': True, 'prediction': False})
+simulator.plot.show('scene')
+simulator.plot.show('input')
+
+# run it!
+simulator.run()
+
+# show/save some results
+simulator.plot.show_movie('scene', repeat=False)
+# simulator.plot.save_movie('input', number_of_frames=4)
+# simulator.plot.save('a', time=3)
