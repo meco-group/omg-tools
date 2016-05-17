4f880a8d2f0274865d1e79475add498f70ace24e
rubenvanparys <ruben.vanparys@kuleuven.be>
2016-05-17 21:49:45 +0200

    added bicycle, agv, agv2 to travis test

--- examples/p2p_bicycle.py
+++ examples/p2p_bicycle.py
@@ -39,11 +39,8 @@ environment.add_obstacle(Obstacle({'position': [1., 1.]}, shape=Circle(0.5),
 # create a point-to-point problem
 problem = Point2point(vehicle, environment, freeT=True)
 # extra solver settings which may improve performance
-problem.set_options({'solver_options': {'ipopt': {'ipopt.linear_solver': 'ma57'}}})
-# problem.set_options({'solver_options': {'ipopt': {'ipopt.hessian_approximation': 'limited-memory'}}})
-# problem.set_options({'solver_options': {'ipopt': {'ipopt.warm_start_bound_push': 1e-6}}})
-# problem.set_options({'solver_options': {'ipopt': {'ipopt.warm_start_mult_bound_push': 1e-6}}})
-# problem.set_options({'solver_options': {'ipopt': {'ipopt.mu_init': 1e-5}}})
+problem.set_options({'solver_options': {'ipopt': {'ipopt.linear_solver': 'ma57',
+                                                  'ipopt.hessian_approximation': 'limited-memory'}}})
 problem.init()
 
 # create simulator
