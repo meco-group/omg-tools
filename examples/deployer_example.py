6f0725f636920916a75c267fad730882f32765fc
rubenvanparys <ruben.vanparys@kuleuven.be>
2016-10-12 20:23:20 +0200

    update

--- examples/deployer_example.py
+++ examples/deployer_example.py
@@ -26,8 +26,6 @@ import numpy as np
 
 # create vehicle
 vehicle = Holonomic()
-vehicle.set_options({'safety_distance': 0.1})
-vehicle.set_options({'ideal_prediction': False})
 vehicle.set_initial_conditions([0., 0.]) # dummy: required for problem.init()
 vehicle.set_terminal_conditions([0., 0.]) # dummy: required for problem.init()
 
