2c62ad7358fef3de04e2624eab0832c9e55cde1d
rubenvanparys <ruben.vanparys@kuleuven.be>
2016-10-14 19:29:25 +0200

    worked out example for paper

--- examples/p2p_quadrotor_3d_2.py
+++ examples/p2p_quadrotor_3d_2.py
@@ -16,48 +16,47 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-import sys, os
-sys.path.insert(0, os.getcwd()+'/..')
+
 from omgtools import *
 
 # create vehicle
 vehicle = Quadrotor3Dv2(0.5)
 
-vehicle.set_initial_conditions([-5., -5., -3, 0., 0., 0., 0., 0.])
-vehicle.set_terminal_conditions([5., 5., 3])
-vehicle.define_knots(knot_intervals=10)
+vehicle.set_initial_conditions([-3, -3, -1, 0, 0, 0, 0, 0])
+vehicle.set_terminal_conditions([3, 3, 1])
+
 vehicle.set_options({'safety_distance': 0.1})
-vehicle.set_options({'substitution': True, 'exact_substitution': False})
+# vehicle.set_options({'substitution': True, 'exact_substitution': False})
 
 # create environment
-environment = Environment(room={'shape': Cube(12.)})
+environment = Environment(room={'shape': Cube(8)})
 
-
-traj1 = {'velocity': {'time': [1.6], 'values': [[0.0, 0.0, 3.]]}}
-traj2 = {'velocity': {'time': [1.6], 'values': [[0.0, 0.0, -3.]]}}
-
-# environment.add_obstacle(Obstacle({'position': [-2., 0., -4.]}, shape=Plate(Rectangle(6., 18.), 0.25, orientation=[0., np.pi/2, 0.]), simulation={'trajectories': traj1}))
-# environment.add_obstacle(Obstacle({'position': [2., 0., 4.]}, shape=Plate(Rectangle(6., 18.), 0.25, orientation=[0., np.pi/2, 0.]), simulation={'trajectories': traj2}))
-
-environment.add_obstacle(Obstacle({'position': [-2., 0., -3]}, shape=Plate(Rectangle(4.5, 14.), 0.25, orientation=[0., np.pi/2, 0.]), simulation={'trajectories': traj1}))
-environment.add_obstacle(Obstacle({'position': [2., 0., 3]}, shape=Plate(Rectangle(4.5, 14.), 0.25, orientation=[0., np.pi/2, 0.]), simulation={'trajectories': traj2}))
+trajectory = {'velocity': {'time': [2], 'values': [[0, 0, -1]]}}
+obst1 = Obstacle({'position': [-2, 0, -1.5]}, shape=Plate(Rectangle(4., 10.), 0.1, orientation=[0., np.pi/2, 0.]))
+obst2 = Obstacle({'position': [2, 0, 3]}, shape=Plate(Rectangle(4., 10.), 0.1, orientation=[0., np.pi/2, 0.]), simulation={'trajectories': trajectory})
+environment.add_obstacle([obst1, obst2])
 
 # create a point-to-point problem
-problem = Point2point(vehicle, environment, freeT=False)
+problem = Point2point(vehicle, environment, freeT=False, options={'horizon_time': 5.})
 problem.set_options({'solver_options': {'ipopt': {'ipopt.linear_solver': 'ma57'}}})
-problem.set_options({'hard_term_con': False, 'horizon_time': 5.})
 problem.init()
 
+vehicle.problem = problem
 # create simulator
 simulator = Simulator(problem)
-vehicle.plot('state', knots=True)
 vehicle.plot('input', knots=True)
-problem.plot('scene', view=[0, -90], axis=True)  # elevation and azimuth of cam
+problem.plot('scene', view=[20, -80], axis=True)  # elevation and azimuth of cam
+
+# vehicle.plot('dd_err', knot=True)
+# vehicle.plot('d_err', knot=True)
+# vehicle.plot('err', knots=True)
 
 # run it!
+# simulator.run_once()
 simulator.run()
 
-problem.plot_movie('scene', number_of_frames=100, repeat=True, view=[30, 60])
+problem.plot_movie('scene', view=[20, -80], number_of_frames=100, repeat=False) # side
+problem.plot_movie('scene', view=[90, -90], number_of_frames=100, repeat=False) # top
 
 # Save a movie as gif: you need imagemagick for this!
 # problem.save_movie('scene', format='gif', name='quad2', view=[25, 60], number_of_frames=100, movie_time=5, axis=False)
