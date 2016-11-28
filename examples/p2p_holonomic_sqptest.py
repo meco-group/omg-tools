acb2d6ce0584ddf51c5167ff56c3e5abbb00b53a
rubenvanparys <ruben.vanparys@kuleuven.be>
2016-11-28 14:46:30 +0100

    update blocksqp example

--- examples/p2p_holonomic_sqptest.py
+++ examples/p2p_holonomic_sqptest.py
@@ -21,8 +21,6 @@ import sys, os
 sys.path.insert(0, os.getcwd()+'/..')
 from omgtools import *
 
-print sys.path
-
 # initial guess via ipopt
 vehicle = Holonomic(options={'safety_distance': 0.1})
 vehicle.set_initial_conditions([-1.5, -1.5])
@@ -46,7 +44,7 @@ rectangle = Rectangle(width=3., height=0.2)
 environment.add_obstacle(Obstacle({'position': [-2.1, -0.5]}, shape=rectangle))
 environment.add_obstacle(Obstacle({'position': [1.7, -0.5]}, shape=rectangle))
 problem1 = Point2point(vehicle, environment, freeT=False)
-problem1.set_options({'solver': 'blocksqp', 'solver_options': {'blocksqp': {'warmstart': True, 'hess_lim_mem': 0, 'print_header': False}}})
+problem1.set_options({'solver': 'blocksqp', 'solver_options': {'blocksqp': {'warmstart': False, 'hess_lim_mem': 0, 'print_header': False}}})
 problem1.init()
 problem1.father._var_result = problem0.father._var_result
 problem1.father._dual_var_result = problem0.father._dual_var_result
@@ -68,3 +66,21 @@ problem2.father._var_result = problem1.father._var_result
 problem2.father._dual_var_result = problem1.father._dual_var_result
 simulator = Simulator(problem2)
 simulator.run_once(simulate=False, init_reset=False)
+
+# use solution as init guess for blocksqp (same problem)
+vehicle = Holonomic(options={'safety_distance': 0.1})
+vehicle.set_initial_conditions([-1.5, -1.5])
+vehicle.set_terminal_conditions([2., 2.])
+environment = Environment(room={'shape': Square(5.)})
+rectangle = Rectangle(width=3., height=0.2)
+environment.add_obstacle(Obstacle({'position': [-2.1, -0.5]}, shape=rectangle))
+environment.add_obstacle(Obstacle({'position': [1.7, -0.5]}, shape=rectangle))
+problem3 = Point2point(vehicle, environment, freeT=False)
+problem3.set_options({'solver': 'blocksqp', 'solver_options': {'blocksqp': {'warmstart': True, 'hess_lim_mem': 0, 'print_header': False}}})
+problem3.init()
+problem3.father._var_result = problem2.father._var_result
+problem3.father._dual_var_result = problem2.father._dual_var_result
+simulator = Simulator(problem3)
+problem3.plot('scene')
+vehicle.plot('input')
+simulator.run(init_reset=False)
