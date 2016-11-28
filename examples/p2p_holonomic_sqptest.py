4b2eed5783e3b7500baa691f3c399594475d7598
rubenvanparys <ruben.vanparys@kuleuven.be>
2016-11-28 10:33:10 +0100

    clean up sqp example

--- examples/p2p_holonomic_sqptest.py
+++ examples/p2p_holonomic_sqptest.py
@@ -21,53 +21,50 @@ import sys, os
 sys.path.insert(0, os.getcwd()+'/..')
 from omgtools import *
 
-# create vehicle
-vehicle = Holonomic()
-vehicle.set_options({'safety_distance': 0.1})
-
-vehicle.set_initial_conditions([-1.5, -1.5])
-vehicle.set_terminal_conditions([2., 2.])
-
-# create environment
-environment = Environment(room={'shape': Square(5.)})
-rectangle = Rectangle(width=3., height=0.2)
-
-environment.add_obstacle(Obstacle({'position': [-2.1, -0.5]}, shape=rectangle))
-environment.add_obstacle(Obstacle({'position': [1.7, -0.5]}, shape=rectangle))
-# trajectories = {'velocity': {'time': [3., 4.],
-#                              'values': [[-0.15, 0.0], [0., 0.15]]}}
-# environment.add_obstacle(Obstacle({'position': [1.5, 0.5]}, shape=Circle(0.4),
-#                                   simulation={'trajectories': trajectories}))
+print sys.path
 
 # initial guess via ipopt
+vehicle = Holonomic(options={'safety_distance': 0.1})
+vehicle.set_initial_conditions([-1.5, -1.5])
+vehicle.set_terminal_conditions([2., 2.])
+environment = Environment(room={'shape': Square(5.)})
+rectangle = Rectangle(width=3., height=0.2)
+environment.add_obstacle(Obstacle({'position': [-2.1, -0.5]}, shape=rectangle))
+environment.add_obstacle(Obstacle({'position': [1.7, -0.5]}, shape=rectangle))
 problem0 = Point2point(vehicle, environment, freeT=False)
 problem0.set_options({'solver': 'ipopt', 'solver_options': {'ipopt': {'ipopt.linear_solver': 'ma57'}}})
-
 problem0.init()
 simulator = Simulator(problem0)
 simulator.run_once(simulate=False)
 
-# online mp via blocksqp
+# use solution as init guess for blocksqp
+vehicle = Holonomic(options={'safety_distance': 0.1})
+vehicle.set_initial_conditions([-1.5, -1.5])
+vehicle.set_terminal_conditions([2., 2.])
+environment = Environment(room={'shape': Square(5.)})
+rectangle = Rectangle(width=3., height=0.2)
+environment.add_obstacle(Obstacle({'position': [-2.1, -0.5]}, shape=rectangle))
+environment.add_obstacle(Obstacle({'position': [1.7, -0.5]}, shape=rectangle))
 problem1 = Point2point(vehicle, environment, freeT=False)
 problem1.set_options({'solver': 'blocksqp', 'solver_options': {'blocksqp': {'warmstart': True, 'hess_lim_mem': 0, 'print_header': False}}})
 problem1.init()
-
 problem1.father._var_result = problem0.father._var_result
 problem1.father._dual_var_result = problem0.father._dual_var_result
 simulator = Simulator(problem1)
 simulator.run_once(simulate=False, init_reset=False)
 
-problem = Point2point(vehicle, environment, freeT=False)
-problem.set_options({'solver': 'blocksqp', 'solver_options': {'blocksqp': {'warmstart': True, 'hess_lim_mem': 0, 'print_header': False}}})
-problem.init()
-
-problem.father._var_result = problem1.father._var_result
-problem.father._dual_var_result = problem1.father._dual_var_result
-simulator = Simulator(problem)
+# use solution as init guess for blocksqp (same problem)
+vehicle = Holonomic(options={'safety_distance': 0.1})
+vehicle.set_initial_conditions([-1.5, -1.5])
+vehicle.set_terminal_conditions([2., 2.])
+environment = Environment(room={'shape': Square(5.)})
+rectangle = Rectangle(width=3., height=0.2)
+environment.add_obstacle(Obstacle({'position': [-2.1, -0.5]}, shape=rectangle))
+environment.add_obstacle(Obstacle({'position': [1.7, -0.5]}, shape=rectangle))
+problem2 = Point2point(vehicle, environment, freeT=False)
+problem2.set_options({'solver': 'blocksqp', 'solver_options': {'blocksqp': {'warmstart': True, 'hess_lim_mem': 0, 'print_header': False}}})
+problem2.init()
+problem2.father._var_result = problem1.father._var_result
+problem2.father._dual_var_result = problem1.father._dual_var_result
+simulator = Simulator(problem2)
 simulator.run_once(simulate=False, init_reset=False)
-
-
-# problem.plot('scene')
-# vehicle.plot('input', knots=True, prediction=True, labels=['v_x (m/s)', 'v_y (m/s)'])
-# simulator.run(init_reset=False)
-
