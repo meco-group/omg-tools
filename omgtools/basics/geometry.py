d20e20abfe3592a5d5f1837967e2346c7978438b
u0098366 <tim.mercy@kuleuven.be>
2017-01-17 12:54:16 +0100

    Adapted geometric functions

--- omgtools/basics/geometry.py
+++ omgtools/basics/geometry.py
@@ -26,7 +26,7 @@ def distance_between_points(point1, point2):
 
 def distance_to_line(point, line):
     # returns the x- and y-direction distance from point to a line
-    # taken from https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+    # based on: https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
     x2, y2, x3, y3 = self.frame['border']['limits']
     # number vertices of border
     # v1--v2
@@ -53,27 +53,30 @@ def distance_to_line(point, line):
     distance[1] = min(abs(dist23), abs(dist34))  # y-direction
     return distance
 
-def ccw(A, B, C):
-    # taken from http://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
+def order_is_ccw(point1, point2, point3):
+    # based on: http://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
     # Determine if three points are listed in a counterclockwise order.
-    # So say you have three points A, B and C. If the slope of the line AB is less than the slope of the line AC
-    # then the three points are listed in a counterclockwise order.
+    # There are three points: point1, point2, point3. If the slope of the line between point1&point2 is
+    # smaller than the slope of the line between point1&point3, 
+    # then the three points have a counterclockwise order.
     
-    return (C[1]-A[1])*(B[0]-A[0]) > (B[1]-A[1])*(C[0]-A[0])
+    return (point3[1]-point1[1])*(point2[0]-point1[0]) > (point2[1]-point1[1])*(point3[0]-point1[0])
 
 def intersect_line_segments(line1, line2):
-    A, B = line1
-    C, D = line2
-    # taken from http://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
-    # Think of two line segments AB, and CD. These intersect if and only if points A and B are
-    # separated by segment CD and points C and D are separated by segment AB. 
-    # If points A and B are separated by segment CD then ACD and BCD should have opposite orientation
-    # meaning either ACD or BCD is counterclockwise but not both.
+    point1, point2 = line1
+    point3, point4 = line2
+    # based on: http://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
+    # There are two line segments, one between point1&point2, one between point2&point3.
+    # The segments only intersect if point1 and point2 are separated by the line segment between
+    # point2 & point3 and point3 & point4 are separated by the segment between point1 & point2. 
+    # If point1 and point 2 are separated by the segment between point3 & point4, then the connection of
+    # point1-point3-point4 and point2-point3-point4 have an opposite order, this means that 
+    # one of both connections is ordered counterclockwise, but noth both.
     
-    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+    return order_is_ccw(point1,point3,point4) != order_is_ccw(point2,point3,point4) and order_is_ccw(point1,point2,point3) != order_is_ccw(point1,point2,point4)
 
 def intersect_lines(line1, line2):
-    # taken from https://en.wikipedia.org/wiki/Line-line_intersection
+    # based on:  https://en.wikipedia.org/wiki/Line-line_intersection
     x1, y1= line1[0]
     x2, y2= line1[1]
     x3, y3= line2[0]
