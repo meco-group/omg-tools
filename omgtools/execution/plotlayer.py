71f5e5d5e096eb46574fdbeeb9f011aad1b20dee
timmercy <tim.mercy@kuleuven.be>
2017-02-27 15:08:58 +0100

    try to fix matplotlib error

--- omgtools/execution/plotlayer.py
+++ omgtools/execution/plotlayer.py
@@ -20,12 +20,19 @@
 import os
 import shutil
 import matplotlib
+matplotlib.use('TKAgg')
+print matplotlib.__version__
+print matplotlib.__version__
+print matplotlib.__version__
+print matplotlib.__version__
+print matplotlib.__version__
+print matplotlib.__version__
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from mpl_toolkits.mplot3d import Axes3D, proj3d
 import numpy as np
 import warnings
-matplotlib.use('TKAgg')
+
 
 # def orthogonal_proj(zfront, zback):
 #     a = (zfront+zback)/(zfront-zback)
