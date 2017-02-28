00045660851ace6cfd7133a46ba347e2292685bd
u0098366 <tim.mercy@kuleuven.be>
2017-02-28 14:05:55 +0100

    Move backend

--- omgtools/execution/plotlayer.py
+++ omgtools/execution/plotlayer.py
@@ -20,12 +20,12 @@
 import os
 import shutil
 import matplotlib
+matplotlib.use('TKAgg')
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from mpl_toolkits.mplot3d import Axes3D, proj3d
 import numpy as np
 import warnings
-matplotlib.use('TKAgg')
 
 # def orthogonal_proj(zfront, zback):
 #     a = (zfront+zback)/(zfront-zback)
