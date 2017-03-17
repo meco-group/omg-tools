f238fc83f00c4255f1cffca0992a2fb88fdf8751
u0098366 <tim.mercy@kuleuven.be>
2017-03-17 16:37:02 +0100

    Check if delay is not bigger than remaining number of setpoints

--- omgtools/execution/deployer.py
+++ omgtools/execution/deployer.py
@@ -55,6 +55,12 @@ class Deployer:
             delay = 0
         else:
             delay = int((current_time - self.current_time - update_time)/self.sample_time)
+        
+        # if the update time + delay is more than the time which is left, leave out the delay
+        if hasattr(self.problem.vehicles[0], 'trajectories'):
+            if (delay + int(np.round(update_time/self.sample_time, 6))) > int(np.round(float(self.problem.vehicles[0].trajectories['time'][:, -1] - self.current_time)/self.sample_time,6)):
+                delay = 0
+                
         self.problem.predict(current_time, update_time, self.sample_time, states, delay)
         self.problem.solve(current_time, update_time)
         self.problem.store(current_time, update_time, self.sample_time)
