05725655e23a4df2b8273af0d507e906160bbd93
u0098366 <tim.mercy@kuleuven.be>
2017-03-23 15:23:20 +0100

    corrected error in update time of deployer

--- omgtools/execution/deployer.py
+++ omgtools/execution/deployer.py
@@ -42,12 +42,12 @@ class Deployer:
         if not update_time:
             update_time = self.update_time
         if hasattr(self.problem.vehicles[0], 'signals'):
-            if update_time - float(self.problem.vehicles[0].signals['time'][:, -1] - self.current_time) > self.sample_time:
+            if round(update_time - float(self.problem.vehicles[0].signals['time'][:, -1] - self.current_time),4) >= self.sample_time:
                 update_time = float(self.problem.vehicles[0].signals['time'][:, -1] - self.current_time)
         # is there enough time left to update with the normal update time? 
         # if not, lower update time
         elif hasattr(self.problem.vehicles[0], 'trajectories'):
-            if update_time - float(self.problem.vehicles[0].trajectories['time'][:, -1] - self.current_time) > self.sample_time:
+            if round(update_time - float(self.problem.vehicles[0].trajectories['time'][:, -1] - self.current_time),4) >= self.sample_time:
                 update_time = float(self.problem.vehicles[0].trajectories['time'][:, -1] - self.current_time)
         if (self.iteration0):
             self.iteration0 = False
