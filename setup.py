fab671577e456a905b9f8e81423df62ff25d8663
rubenvanparys <ruben.vanparys@kuleuven.be>
2017-04-24 09:36:36 +0200

    7th trial

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ def read(fname):
 
 
 setup(
-    name="omg-tools",
-    version="0.1",
-    author="Ruben Van Parys",
-    author_email="ruben.vanparys@kuleuven.be",
+    name='omg-tools',
+    version='0.1',
+    author='Ruben Van Parys',
+    author_email='ruben.vanparys@kuleuven.be',
     description=('Optimal Motion Generation tools: a user-friendly tool for ' +
                  'modeling, simulating and embedding of (spline-based) motion ' +
                  'planning problems'),
     license="LGPLv3",
-    keywords="optimization motion planning splines distributed multi-agent",
-    url="https://github.com/meco-group/omg-tools",
+    keywords='optimization motion planning splines distributed multi-agent',
+    url='https://github.com/meco-group/omg-tools',
     packages=['omgtools', 'omgtools/basics', 'omgtools/environment',
               'omgtools/problems', 'omgtools/execution', 'omgtools/vehicles',
               'omgtools/export'],
@@ -28,9 +28,9 @@ setup(
                        'export/*/*/*/*.cpp', 'export/*/*/*/*.hpp']},
     long_description=read('readme.md'),
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
     ],
 )
