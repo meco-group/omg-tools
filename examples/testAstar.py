470cc61f96578eeb76d5aff11d3971dd9e509290
u0098366 <tim.mercy@kuleuven.be>
2017-06-20 15:45:50 +0200

    update simple Astart example

--- examples/testAstar.py
+++ examples/testAstar.py
@@ -1,5 +1,21 @@
-import os, sys
-sys.path.insert(0, os.getcwd()+'/..')
+# This file is part of OMG-tools.
+#
+# OMG-tools -- Optimal Motion Generation-tools
+# Copyright (C) 2016 Ruben Van Parys & Tim Mercy, KU Leuven.
+# All rights reserved.
+#
+# OMG-tools is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 from omgtools import *
 
@@ -12,7 +28,7 @@ square_size = 0.5
 environment = Environment(room={'shape': Rectangle(width=width, height=height), 'position':[5,5]})
 environment.add_obstacle(Obstacle({'position': [5, 5]}, shape=Rectangle(width=2,height=2)))
 
-planner = AStarPlanner(environment, cell_size=square_size, start=start, goal=goal)
+planner = AStarPlanner(environment, [10,10], start, goal)
 waypoints = planner.get_path()
 planner.grid.draw()
 planner.plot_path(waypoints)
