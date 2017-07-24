8fd27a70c333afe391b381de3b192713ebeab135
u0098366 <tim.mercy@kuleuven.be>
2017-07-24 17:01:50 +0200

    Change radius, remove ma57

--- examples/p2p_multiframeproblem_dubins.py
+++ examples/p2p_multiframeproblem_dubins.py
@@ -21,7 +21,7 @@
 from omgtools import *
 
 # create vehicle
-vehicle = Dubins(bounds={'vmax': 0.7, 'wmax': np.pi/3., 'wmin': -np.pi/3.}, # in rad/s
+vehicle = Dubins(shapes=Circle(radius=0.3), bounds={'vmax': 0.7, 'wmax': np.pi/3., 'wmin': -np.pi/3.}, # in rad/s
                  options={'substitution': False})
 vehicle.define_knots(knot_intervals=10)
 
@@ -50,7 +50,8 @@ globalplanner = AStarPlanner(environment, [10,10], start, goal)
 # make coordinator
 options={'freeT': True, 'horizon_time': 15}
 multiproblem=MultiFrameProblem(vehicle, environment, globalplanner, options=options, frame_type='min_nobs')
-multiproblem.set_options({'solver_options': {'ipopt': {'ipopt.linear_solver': 'ma57',
+multiproblem.set_options({'solver_options': {'ipopt': {
+													   # 'ipopt.linear_solver': 'ma57',
                                                        'ipopt.hessian_approximation': 'limited-memory'}}})
 multiproblem.init()
 
