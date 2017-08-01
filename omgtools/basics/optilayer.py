721fea7d5d1640c7087da0256cd3c5dacfd334a0
u0098366 <tim.mercy@kuleuven.be>
2017-08-01 14:45:15 +0200

    Add argument indicating for how many segments to shift variables

--- omgtools/basics/optilayer.py
+++ omgtools/basics/optilayer.py
@@ -451,18 +451,22 @@ class OptiFather(object):
                     _init_tf[basis] = init_dual_transform(basis)
                 child._splines_dual[name]['init'] = _init_tf[basis]
 
-    def transform_primal_splines(self, transform_fun):
+    def transform_primal_splines(self, transform_fun, n_seg_shift=None):
+        # n_seg_shift: indicates the amount of segments of which the variables must be shifted
         for label, child in self.children.items():
             for name, spl in child._splines_prim.items():
                 if name in child._variables:
-                    basis = spl['basis']
-                    init = spl['init']
-                    if init is not None:
-                        self._var_result[label, name] = transform_fun(
-                            self._var_result[label, name], basis, init)
-                    else:
-                        self._var_result[label, name] = transform_fun(
-                            self._var_result[label, name], basis)
+                    # check if n in 'segn' is smaller than n_seg_shift, to decide if shift is necessary
+                    if (n_seg_shift is not None and 'seg' in name and
+                    int(name[name.index('seg')+3]) < n_seg_shift):
+                        basis = spl['basis']
+                        init = spl['init']
+                        if init is not None:
+                            self._var_result[label, name] = transform_fun(
+                                self._var_result[label, name], basis, init)
+                        else:
+                            self._var_result[label, name] = transform_fun(
+                                self._var_result[label, name], basis)
 
     def transform_dual_splines(self, transform_fun):
         for label, child in self.children.items():
