821bc83b79eb88b1d2342792d1172d6fca27e5cb
u0098366 <tim.mercy@kuleuven.be>
2017-08-01 14:51:18 +0200

    room limits for multiple segments

--- omgtools/vehicles/vehicle.py
+++ omgtools/vehicles/vehicle.py
@@ -113,7 +113,7 @@ class Vehicle(OptiChild, PlotLayer):
         return self.splines
 
     def define_collision_constraints_2d(self, hyperplanes,
-        room_limits, positions, horizon_time, tg_ha=0, offset=0):
+        room, positions, horizon_time, tg_ha=0, offset=0):
         t = self.define_symbol('t')
         # T = self.define_symbol('T')
         safety_distance = self.options['safety_distance']
@@ -155,29 +155,32 @@ class Vehicle(OptiChild, PlotLayer):
             # then decide on type of constraints to use:
             # room_limits or hyperplanes
             if self.options['room_constraints']:
-                    if ((isinstance(room['shape'], (Rectangle, Square)) and
-                        room['shape'].orientation == 0.0) and
-                        (isinstance(shape, Circle) or
-                        (isinstance(shape, (Rectangle, Square)) and
-                         shape.orientation == 0)) and
-                        (isinstance(tg_ha, (int, float, long)) and tg_ha == 0.)):
-                        for chck in checkpoints:
-                            for k in range(2):
-                                self.define_constraint(-(chck[k]+position[k]) + room_limits[k][0] + rad[0], -inf, 0.)
-                                self.define_constraint((chck[k]+position[k]) - room_limits[k][1] + rad[0], -inf, 0.)
-                    else:
-                        hyp_room = room['shape'].get_hyperplanes(position = room['position'])
-                        for l, chck in enumerate(checkpoints):
-                            for hpp in hyp_room.itervalues():
-                                con = 0
-                                con += (hpp['a'][0]*chck[0] + hpp['a'][1]*chck[1])*(1.-tg_ha**2)
-                                con += (-hpp['a'][0]*chck[1] + hpp['a'][1]*chck[0])*(2*tg_ha)
-                                pos = [0, 0]  # next part gives an offset to input position e.g. for trailer position
-                                pos[0] = position[0]*(1+tg_ha**2) + offset*(1-tg_ha**2)  # = real_pos*(1+tg_ha**2)
-                                pos[1] = position[1]*(1+tg_ha**2) + offset*(2*tg_ha)  # = real_pos*(1+tg_ha**2)
-                                con += (hpp['a'][0]*pos[0] + hpp['a'][1]*pos[1])
-                                con += (-hpp['b']+rad[l])*(1+tg_ha**2)
-                                self.define_constraint(con, -inf, 0)
+                lims = room['shape'].get_canvas_limits()
+                room_limits = []
+                room_limits += [lims[k]+room['position'][k] for k in range(self.n_dim)]
+                if ((isinstance(room['shape'], (Rectangle, Square)) and
+                    room['shape'].orientation == 0.0) and
+                    (isinstance(shape, Circle) or
+                    (isinstance(shape, (Rectangle, Square)) and
+                     shape.orientation == 0)) and
+                    (isinstance(tg_ha, (int, float, long)) and tg_ha == 0.)):
+                    for chck in checkpoints:
+                        for k in range(2):
+                            self.define_constraint(-(chck[k]+position[k]) + room_limits[k][0] + rad[0], -inf, 0.)
+                            self.define_constraint((chck[k]+position[k]) - room_limits[k][1] + rad[0], -inf, 0.)
+                else:
+                    hyp_room = room['shape'].get_hyperplanes(position = room['position'])
+                    for l, chck in enumerate(checkpoints):
+                        for hpp in hyp_room.itervalues():
+                            con = 0
+                            con += (hpp['a'][0]*chck[0] + hpp['a'][1]*chck[1])*(1.-tg_ha**2)
+                            con += (-hpp['a'][0]*chck[1] + hpp['a'][1]*chck[0])*(2*tg_ha)
+                            pos = [0, 0]  # next part gives an offset to input position e.g. for trailer position
+                            pos[0] = position[0]*(1+tg_ha**2) + offset*(1-tg_ha**2)  # = real_pos*(1+tg_ha**2)
+                            pos[1] = position[1]*(1+tg_ha**2) + offset*(2*tg_ha)  # = real_pos*(1+tg_ha**2)
+                            con += (hpp['a'][0]*pos[0] + hpp['a'][1]*pos[1])
+                            con += (-hpp['b']+rad[l])*(1+tg_ha**2)
+                            self.define_constraint(con, -inf, 0)
 
     def define_collision_constraints_3d(self, hyperplanes, environment, positions):
         # orientation for 3d not yet implemented!
