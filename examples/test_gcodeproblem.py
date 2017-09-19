35a8cdc90398cc3b41ab87da38fcff2ed1c8a3ab
u0098366 <tim.mercy@kuleuven.be>
2017-09-19 14:13:10 +0200

    Use deployer or simulator

--- examples/test_gcodeproblem.py
+++ examples/test_gcodeproblem.py
@@ -43,7 +43,9 @@ schedulerproblem = GCodeSchedulerProblem(tool, GCode, n_segments=n_blocks, with_
 schedulerproblem.set_options({'solver_options': {'ipopt': {'ipopt.tol': 1e-8,
 														   'ipopt.linear_solver': 'ma57'}}})#,
                                                            #'ipopt.hessian_approximation': 'limited-memory'}}})
-# put problem in simulator
+# put problem in deployer: choose this if you just want to obtain the trajectories for the tool
+deployer = Deployer(schedulerproblem, sample_time=0.001)
+# put problem in simulator: choose this if you want to simulate step by step, and investigate after each segment
 simulator = Simulator(schedulerproblem, sample_time=0.001)
 
 # define what you want to plot
@@ -53,9 +55,11 @@ tool.plot('input', knots=True, prediction=True, labels=['v_x (m/s)', 'v_y (m/s)'
 tool.plot('dinput', knots=True, prediction=True, labels=['a_x (m/s^2)', 'a_y (m/s^2)', 'a_z (m/s^2)'])
 tool.plot('ddinput', knots=True, prediction=True, labels=['j_x (m/s^3)', 'j_y (m/s^3)', 'j_z (m/s^3)'])
 
-# simulate using a receding horizon of one segment
-simulator.run_segment()
+# run using a receding horizon of one segment
+deployer.run_segment()
+# simulator.run_segment()
 
-schedulerproblem.plot_movie('scene', number_of_frames=100, repeat=False)
-schedulerproblem.save_movie('scene', format='gif', name='gcodegif', number_of_frames=100, movie_time=10, axis=False)
+# plotting and saving afterwards is only available when using the simulator
+# schedulerproblem.plot_movie('scene', number_of_frames=100, repeat=False)
+# schedulerproblem.save_movie('scene', format='gif', name='gcodegif', number_of_frames=100, movie_time=10, axis=False)
 # schedulerproblem.save_movie('scene', number_of_frames=50, name='gcodescheduler', axis=False)
