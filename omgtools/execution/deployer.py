af1c2a5204c14be90a6dcd4329e01fd3184e5057
u0098366 <tim.mercy@kuleuven.be>
2017-09-20 11:23:40 +0200

    more general check if final position is reached

--- omgtools/execution/deployer.py
+++ omgtools/execution/deployer.py
@@ -185,7 +185,7 @@ class Deployer:
                 plt.pause(0.1)
 
                 # check if target is reached
-                if (np.linalg.norm(self.problem.goal_state-state_traj[:, -1]) < 1e-2 and np.linalg.norm(input_traj[:, -1]) < 1e-2):
+                if (np.linalg.norm(self.problem.segments[0]['end']-state_traj[:, -1]) < 1e-2 and np.linalg.norm(input_traj[:, -1]) < 1e-2):
                     target_reached = True
 
         # target reached, print final information
