28f76ee45a33f4ea06b59abcaf1237f43146de59
u0098366 <tim.mercy@kuleuven.be>
2017-12-21 16:38:13 +0100

    convert svg to gcode

--- omgtools/gui/svg_reader.py
+++ omgtools/gui/svg_reader.py
@@ -322,3 +322,321 @@ class SVGReader(object):
         self.convert_lines()  # looks for shapes defined by polyline and line
         # if you found some paths, they are transformed to an obstacle and
         # added to self.obstacles
+
+    def get_gcode_description (self):
+        # Note: for now this function only works for lines and paths. With capital M and lower-case c.
+        # The paths are supposed to represent circle segments, if they don't,
+        # a circle approximation of the curve is used.
+
+        children = self.tree.getchildren()  # the xml-tree
+        self.commands = []  # holds the GCode commands
+
+        for idx, child in enumerate(children):
+            if 'line' in child.tag:
+                # child is a line
+                # example: <line fill="none" stroke="#333333" stroke-width="10" x1="25.41" y1="174.59" x2="69.672" y2="174.59"/>
+                x1, y1 = float(child.get('x1')), float(child.get('y1'))  # start
+                x2, y2 = float(child.get('x2')), float(child.get('y2'))  # end
+                # add transform
+                x1 += self.transform[0]
+                y1 += self.transform[1]
+                x2 += self.transform[0]
+                y2 += self.transform[1]
+
+                y1 = -y1  # flip axis: in svg top left corner is [0,0], y-axis points downwards
+                y2 = -y2  # make y-axis point upwards
+
+                # make line GCode segment
+                if not self.commands:
+                    # this is the first command, so make it a G00
+                    self.commands.append('G00 X'+str(x1)+' Y'+str(y1))
+                self.commands.append('G01 X'+str(x2)+' Y'+str(y2))  # only add endpoint, startpoint comes from previous command
+
+            elif 'path' in child.tag:
+                path = child.get('d')
+                # d='Mx,y c x1 y1 x2 y2 x y'
+                # Mx,y = the startpoint or endpoint of the curve
+                # c starts a curve, lower case means relative coordinates i.e. relative to Mx, y
+                # x1 y1 is the control point that is closest to Mx, y
+                # x2 y2 is the second control point
+                # x y is the endpoint of the curve
+                # d= can contain multiple c-commands = curves
+                if path[0] == 'M':
+                    path = path[1:].replace(","," ")  # replace comma by: space
+                    path = path.replace("-"," -")  # replace minus sign by: space minus
+                    path = path.replace("c"," c ")  # replace c by: space c space
+                    path = path.split(' c ')
+
+                    filtered_path = []
+                    for curve in path:
+                        curve = curve.split(' ')
+                        # remove all empty strings
+                        curve = [e for e in curve if e!= '']
+                        curve = [e for e in curve if e!= ' ']
+                        filtered_path.append(curve)  # save filtered path
+
+                    # the first border point of the curve (later decide if this is start or end)
+                    curve_point1 = filtered_path[0]
+                    curve_point1[0] = float(curve_point1[0])
+                    curve_point1[1] = -float(curve_point1[1])  # minus: let y-axis point up
+                    filtered_path.pop(0)  # remove first point from path
+
+                    if filtered_path:
+                        # there are curves in the path,
+                        # loop over all curves
+                        circle_points = []
+                        for curve in filtered_path:
+                            if not circle_points:
+                                # first circle point, move starting from Mx, y = curve_point1
+                                # note: minus sign for y-direction
+                                circle_points.append([float(curve[-2])+curve_point1[0], -float(curve[-1])+curve_point1[1]])
+                            else:
+                                # this was not the first circle point, move relative from previous point
+                                # note: minus sign for y-direction
+                                circle_points.append([float(curve[-2])+circle_points[-1][0], -float(curve[-1])+circle_points[-1][1]])
+
+                        # add endpoint of curve
+                        circle_points.insert(0,curve_point1)
+
+                        # For now this function supposes that each curve consists of minimum three points.
+                        # If not, you need to approximate the circle shape in another way
+
+                        # if len(circle_points) == 2:
+                        #     # compute circle center when only one curve is present
+                        #     # do this by solving a system with 3 equations in unknowns: xc, yc, r
+                        #     # curve_point1 and curve_point2 have to lie on the circle:
+                        #     # (x1-xc)**2+(y1-yc)**2-r**2=0
+                        #     # (x2-xc)**2+(y2-yc)**2-r**2=0
+                        #     # the normal to the connection between (x1,y1) and control point has to pass through xc, yc:
+                        #     # a*xc+b*yc+c=0
+
+                        #     control_point1 = np.array(curve_point1) + np.array([float(curve[0]), -float(curve[1])])
+                        #     curve_point2 = circle_points[-1]
+                        #     control_point2 = np.array(curve_point2) - np.array([float(curve[2]), -float(curve[3])])
+
+                        #     # (y-y1) = (y2-y1)/(x2-x1)(x-x1) --> ax+by+c=0
+
+                        #     x1 = curve_point1[0]
+                        #     y1 = curve_point1[1]
+                        #     x2 = curve_point2[0]
+                        #     y2 = curve_point2[1]
+                        #     a = (x2-x1)
+                        #     b = (y2-y1)
+                        #     c = x1*(x2-x1)-y1*(y2-y1)
+
+                        #     cx = (b*x1**2 + b*y1**2 - b*x2**2 - b*y2**2 + 2*c*y1 - 2*c*y2)/(2*(-a*y1 + a*y2 + b*x1 - b*x2))
+                        #     cy = (-a*x1**2 - a*y1**2 + a*x2**2 + a*y2**2 - 2*c*x1 + 2*c*x2)/(2*(-a*y1 + a*y2 + b*x1 - b*x2))
+
+                        #     r = np.sqrt((x1-cx)**2+(y1-cy)**2)
+
+                        #     # # do this by finding the intersection of the normals on the connection
+                        #     # # between the end points of the curve and their respective control point
+                        #     # curve = filtered_path[0]
+
+                        #     # control_point1 = np.array(curve_point1) + np.array([float(curve[0]), -float(curve[1])])
+                        #     # curve_point2 = circle_points[-1]
+                        #     # control_point2 = np.array(curve_point2) - np.array([float(curve[2]), -float(curve[3])])
+
+                        #     # p1 = curve_point1
+                        #     # p2 = control_point1
+                        #     # p3 = curve_point2
+                        #     # p4 = control_point2
+
+                        #     # if (p1[0] == p2[0] and p3[0] == p4[0]):
+                        #     #     # vertical lines between end and control points
+                        #     #     cx = (p1[0] + p3[0])*0.5
+                        #     #     cy = p1[1]  # = p3[1]
+                        #     # elif (p1[1] == p2[1] and p3[1] == p4[1]):
+                        #     #     # horizontal lines between and and control points
+                        #     #     cx = p1[0]  # p3[0]
+                        #     #     cy = (p1[1] + p3[1])*0.5
+                        #     # elif (p1[0] == p2[0] and p3[1] == p4[1]):
+                        #     #     cx = p3[0]
+                        #     #     cy = p1[1]
+                        #     # elif (p1[1] == p2[1] and p3[0] == p4[0]):
+                        #     #     cx = p1[0]
+                        #     #     cy = p3[1]
+                        #     # elif p2[0] == p1[0]:
+                        #     #     # vertical line1
+                        #     #     cy = p1[1]
+
+                        #     #     rico2 = (p4[1]-p3[1])/(p4[0]-p3[0])
+                        #     #     normal2 = -1/rico2
+                        #     #     cx = (cy-p3[1])/normal2 + p3[0]
+                        #     # elif p2[1] == p1[1]:
+                        #     #     # horizontal line1
+                        #     #     cx = p1[0]
+
+                        #     #     rico2 = (p4[1]-p3[1])/(p4[0]-p3[0])
+                        #     #     normal2 = -1/rico2
+                        #     #     cy = p3[1] + normal2*(cx-p3[0])
+                        #     # elif p3[0] == p4[0]:
+                        #     #     # vertical line1
+                        #     #     cy = p3[1]
+
+                        #     #     rico2 = (p2[1]-p1[1])/(p2[0]-p1[0])
+                        #     #     normal2 = -1/rico2
+                        #     #     cx = (cy-p1[1])/normal2 + p1[0]
+                        #     # elif p3[1] == p4[1]:
+                        #     #     # horizontal line1
+                        #     #     cx = p3[0]
+
+                        #     #     rico2 = (p2[1]-p1[1])/(p2[0]-p1[0])
+                        #     #     normal2 = -1/rico2
+                        #     #     cy = p1[1] + normal2*(cx-p1[0])
+                        #     # else:
+                        #     #     rico1 = (p2[1]-p1[1])/(p2[0]-p1[0])
+                        #     #     normal1 = -1/rico1
+                        #     #     #y = mid1[1] + normal1*(x-mid1[0]) [1]
+                        #     #     rico2 = (p4[1]-p3[1])/(p4[0]-p3[0])
+                        #     #     normal2 = -1/rico2
+                        #     #     #y = mid2[1] + normal2*(x-mid2[0]) [2]
+
+                        #     #     # [1] = [2] --> x =
+                        #     #     if normal2 != normal1:
+                        #     #         cx = np.round((p1[1]-p3[1]-normal1*p1[0]+normal2*p3[0])/(normal2-normal1),1)
+                        #     #     else:
+                        #     #         raise RuntimeError('Normals are equal, something went wrong')
+
+                        #     #     cy = np.round(p1[1] + normal1*(cx-p1[0]),1)
+
+                        #     # r = np.round(np.sqrt((p1[0]-cx)**2+(p1[1]-cy)**2),1)
+
+                        #     # plot solution
+                        #     plt.figure(11)
+                        #     eval = np.linspace(0,2*np.pi,100)
+                        #     plt.plot(cx+r*np.cos(eval),cy+r*np.sin(eval),'g-')
+                        #     plt.plot(cx,cy,'gx')
+                        #     plt.plot(p1[0],p1[1],'rx')
+                        #     # plt.plot(p2[0],p2[1],'rx')
+                        #     plt.plot(p3[0],p3[1],'rx')
+                        #     # plt.plot(p4[0],p4[1],'rx')
+
+                        if len(circle_points) > 2:
+                            # find circle through first three points
+                            # by finding the intersection of the two perpendicular bisectors through [p1p2] and [p2p3]
+                            p1, p2, p3 = circle_points[:3]
+                            mid1 = [(p2[0]+p1[0])*0.5, (p2[1]+p1[1])*0.5]  # midpoint of first bisector
+                            mid2 = [(p3[0]+p2[0])*0.5, (p3[1]+p2[1])*0.5]
+                            cx, cy = [], []  # will hold circle center
+
+                            if (p1[0] == p2[0] and p2[1] == p3[1]):
+                                # vertical and horizontal bisectors
+                                cx = mid1[0]
+                                cy = mid2[1]
+                            elif (p1[1] == p2[1] and p2[0] == p3[0]):
+                                # horizontal and vertical bisectors
+                                cx = mid2[0]
+                                cy = mid1[1]
+                            elif p2[0] == p1[0]:
+                                # vertical bisector1
+                                cx = mid1[0]
+                                rico2 = (p3[1]-p2[1])/(p3[0]-p2[0])
+                                normal2 = -1/rico2
+                                cy = mid2[1] + normal2*(x-mid2[0])
+                            elif p2[1] == p1[1]:
+                                # horizontal bisector1
+                                cy = mid1[1]
+                                rico2 = (p3[1]-p2[1])/(p3[0]-p2[0])
+                                normal2 = -1/rico2
+                                cx = (cy-mid2[1])/normal2 + mid2[0]
+                            elif p2[0] == p3[0]:
+                                # vertical bisector2
+                                cx = mid2[0]
+                                rico1 = (p2[1]-p1[1])/(p2[0]-p1[0])
+                                normal1 = -1/rico1
+                                cy = mid1[1] + normal1*(x-mid1[0])
+                            elif p2[1] == p3[1]:
+                                # horizontal bisector2
+                                cy = mid2[1]
+                                rico1 = (p2[1]-p1[1])/(p2[0]-p1[0])
+                                normal1 = -1/rico1
+                                cx = (cy-mid1[1])/normal1 + mid1[0]
+                            else:
+                                # two diagonal bisectors
+                                rico1 = (p2[1]-p1[1])/(p2[0]-p1[0])
+                                normal1 = -1/rico1
+                                # y = mid1[1] + normal1*(x-mid1[0]) [1]
+                                rico2 = (p3[1]-p2[1])/(p3[0]-p2[0])
+                                normal2 = -1/rico2
+                                # y = mid2[1] + normal2*(x-mid2[0]) [2]
+
+                                # [1] = [2] --> x =
+                                if normal2 != normal1:
+                                    cx = (mid1[1]-mid2[1]-normal1*mid1[0]+normal2*mid2[0])/(normal2-normal1)
+                                else:
+                                    raise RuntimeError('Normals are equal, something went wrong')
+
+                                cy = mid1[1] + normal1*(cx-mid1[0])
+
+                            # compute radius
+                            r = np.sqrt((p1[0]-cx)**2+(p1[1]-cy)**2)
+
+                            # plot solution
+                            # plt.figure(11)
+                            # eval = np.linspace(0,2*np.pi,100)
+                            # plt.plot(cx+r*np.cos(eval),cy+r*np.sin(eval),'g-')
+                            # plt.plot(cx,cy,'gx')
+                            # # plt.plot(mid1[0],mid1[1],'rx')
+                            # # plt.plot(mid2[0],mid2[1],'rx')
+                            # plt.plot(p2[0],p2[1],'rx')
+                            # plt.plot(p1[0],p1[1],'rx')
+                            # plt.plot(p3[0],p3[1],'rx')
+
+                        else:
+                            raise RuntimeError('Curve must consist of more than two points for the moment')
+
+                        # given radius and center
+                        # compute I an J from center
+
+                        # compute end point of curves
+                        curve_point2 = np.array(curve_point1)
+                        for curve in filtered_path:
+                            # add relative positions of all curves
+                            curve_point2 += np.array([float(curve[-2]), -float(curve[-1])])
+
+                        # now decide what is start and end of curve, because start must connect to end of previous command
+                        prev_seg_end = [float(self.commands[-1].split(' ')[1].split('X')[1]),
+                                        float(self.commands[-1].split(' ')[2].split('Y')[1])]
+                        dist1 = np.sqrt((curve_point1[0]-prev_seg_end[0])**2+(curve_point1[1]-prev_seg_end[1])**2)
+                        dist2 = np.sqrt((curve_point2[0]-prev_seg_end[0])**2+(curve_point2[1]-prev_seg_end[1])**2)
+                        # curve start point is closest to the end of the previous segment
+                        if dist1 < dist2:
+                            start = curve_point1
+                            end_curve = curve_point2
+                            control_point = [start[0]+float(filtered_path[0][0]), start[1]-float(filtered_path[0][1])]
+                        else:
+                            start = curve_point2
+                            end_curve = curve_point1
+                            control_point = [start[0]-float(filtered_path[-1][4])+float(filtered_path[-1][2]),
+                                             start[1]+float(filtered_path[-1][5])-float(filtered_path[-1][3])]
+
+                        # compute I and J
+                        I = cx - start[0]
+                        J = cy - start[1]
+
+                        # determine if circle goes clockwise or counter-clockwise
+                        # by taking the vector product of the center to the start point & start to control point
+                        v1 = [start[0]-cx, start[1]-cy]
+                        v2 = [control_point[0]-start[0], control_point[1]-start[1]]
+                        vector_product = v1[0]*v2[1] - v2[0]*v1[1]
+
+                        if vector_product < 0:
+                            # clockwise arc
+                            self.commands.append('G02 X'+str(end_curve[0])+' Y'+str(end_curve[1])+ ' I'+str(I)+' J'+str(J))
+                        else:
+                            # counter-clockwise arc
+                            self.commands.append('G03 X'+str(end_curve[0])+' Y'+str(end_curve[1])+ ' I'+str(I)+' J'+str(J))
+                    else:
+                        # there are no curves in the path, probably it just contains a move command
+                        # so it represents a GCode line segment
+                        self.commands.append('G01 X'+str(end_curve[0])+' Y'+str(end_curve[1]))
+                else:
+                    raise RuntimeError('Only absolute positioning of the start of the curve is supported for now')
+
+        # Write .nc file
+        old_name = self.data.name.split('/')[-1][:-4]
+        f = open(old_name+'_gcode.nc', 'w')
+        for command in self.commands:
+            f.write(command + '\n')
+        f.close()
